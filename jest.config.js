65cc12ac5eb89c70c36fded10edc97b11a9dc3c6
Diego Weinmann <diego.weinmann@hotmail.com>
2020-10-13 21:30:55 -0300

    chore: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/main/**',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest"
+    '.+\\.ts$': 'ts-jest'
   }
-};
+}
